4a49bd70e0f551507cd9e39146fccbfde49644ae
近藤元気 <genki7onepiece@gmail.com>
2023-09-12 22:41:21 +0900

    no message

--- Landmark/Landmark/Models/Landmark.swift
+++ Landmark/Landmark/Models/Landmark.swift
@@ -19,6 +19,7 @@ struct Landmark: Hashable, Codable, Identifiable{
     var category: Category
     var isFavorite: Bool
     
+    
     var locationCoordinate: CLLocationCoordinate2D {
         CLLocationCoordinate2D(
             latitude: coordinates.latitude,
